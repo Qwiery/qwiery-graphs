547e845a22142de47ba7fc16124071a094ddf3bf
Swa <swa@orbifold.net>
2023-12-19 08:07:20 +0100

    Missing index fie.

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export * from "~/constants";
+export * from "~/forest";
+export * from "~/graph";
+export * from "~/graphUtils";
+export * from "~/iEdgeBase";
+export * from "~/iid";
+export * from "~/iNodeBase";
+export * from "~/tree";
+export * from "~/treeNode";
+ 
